afad8cdd0040b05f1511ed29a31f68edbdf4529e
IJustReallyLikeSpecksOfDust <max.falconerware@education.nsw.gov.au>
2025-05-13 12:21:05 +1000

    well, due to poor luck i did not get to test any of this.

--- 99CT_Brech_Moject/main.py
+++ 99CT_Brech_Moject/main.py
@@ -6,6 +6,7 @@ from pybricks.parameters import Port, Stop, Direction, Button, Color
 from pybricks.tools import wait, StopWatch, DataLog
 from pybricks.robotics import DriveBase
 from pybricks.media.ev3dev import SoundFile, ImageFile
+import random
 
 
 # This program requires LEGO EV3 MicroPython v2.0 or higher.
@@ -21,9 +22,20 @@ obstacle_sensor = UltrasonicSensor(Port.S4)
 robot = DriveBase(left_motor, right_motor, wheel_diameter=55.5, axle_track=104)
 line_sensor = ColorSensor(Port.S3)
 claw_motor = Motor(Port.D)
+BLACK = 9
+WHITE = 85
+threshold = (BLACK + WHITE) / 2
+DRIVE_SPEED = 100
+PROPORTIONAL_GAIN = 1.2
 
 
+def closeClaw():  
+    claw_motor.run_until_stalled(-200,then=Stop.COAST, duty_limit=10)
+    ev3.speaker.beep()
 
+def openClaw():  
+    claw_motor.run_until_stalled(200,then=Stop.COAST, duty_limit=10)
+    ev3.speaker.beep()
 
 def followLine(): #code for making the robot follow the line
     ev3.speaker.beep()
@@ -34,58 +46,31 @@ def followLine(): #code for making the robot follow the line
         wait(10)
     ev3.speaker.beep()
 
-def turn(): #code for making the robot turn and then move
-    ev3.speaker.beep()
-    robot.turn(90)
-    ev3.speaker.beep()
-    robot.straight(435)
-    ev3.speaker.beep()
-    robot.turn(90)
-    ev3.speaker.beep()
 
-def openClaw(): #code for opening the claw.
-    claw_motor.run_until_stalled(200,then=Stop.COAST, duty_limit=50)
-    ev3.speaker.beep()
-    claw_motor.reset_angle(0)
-    claw_motor.run_target(200, -90)
-    
-def closeClaw():  #code for closing the claw, ideally trapping a block inside.
-    claw_motor.run_until_stalled(-200,then=Stop.COAST, duty_limit=50)
-    ev3.speaker.beep()
-
-def errorTest():
-if 
-
-# Write your program here.
-ev3.speaker.beep()
-ev3.speaker.beep()
-
-allisSwell = 1 #this determines the mode that the program takes. either planned course or trial and
-funnyvariablename =
-
-#actual process
  
-closeClaw() #set it to a default at the beginning of the code
-if allisSwell == 1:
+followLine() #make this one go to start
+robot.turn(90)
+openClaw() #deposit)
+robot.turn(90)  
+followLine() #to the yellow
+robot.turn(30)
+closeClaw()
+robot.turn(150)
+followLine() #make this one go to start
+robot.turn(90)
+openClaw() #deposit)
+#in theory this will work, otherwise
+if obstacle_sensor.distance() > 100:
+    allisSwell = 2
 
-    followLine() #make this go to red
-    robot.turn(90)
-    openClaw()
-    robot.straight(150) #value can be changed as needed
-    closeClaw()
-    robot.turn(90)
-    followLine() #make this one go to start
-    robot.turn(90)
-    openClaw() #deposit)
-    robot.turn(90)  
-    followLine() #to the yellow
-    robot.turn(30)
-    closeClaw()
-    robot.turn(150)
-    followLine() #make this one go to start
-    robot.turn(90)
-    openClaw() #deposit)
-    #in theory this will work
+while allisSwell == 2: #a failsafe
+    while obstacle_sensor.distance() > 130:
+        rng = random.randint(1,4)
+        robot.drive(100)
+        if rng == 3:
+            robot.turn(90)
+        elif rng == 4:
+            robot.turn(-90)
+        ev3.speaker.beep()
 
-while allisSwell == 2:
     
